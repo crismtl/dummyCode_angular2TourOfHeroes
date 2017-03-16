b8ec6c84e335415a71116e156eac6e78aa6b125c
Cristian Toaquiza <crissmtl@gmail.com>
2017-03-15 22:16:48 -0500

    Agregar rutas al ejemplo de app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [HeroService]
 })
 
 export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroesSlowsly().then(heroes => this.heroes = heroes);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 import { HeroService } from './hero.service';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
